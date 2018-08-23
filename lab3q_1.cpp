dace710abacfd0bbbf6ccc0c3a66905f0ea227f2
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-08-23 17:20:52 +0000

    Add files via upload

---
+++ lab3q_1.cpp
@@ -0,0 +1,20 @@
+//Library
+#include <iostream>
+using namespace std;
+//write the main function
+int main() {
+//declaring the variables
+int a=1;
+char b=2;
+float c=2.007;
+double d=2.01;
+bool e=0;
+//will gett the size of the variable
+cout <<"size of int;"<<sizeof(int)<<"byte"<<"contents of a="<<a<<endl;
+cout <<"size of char;"<<sizeof(char)<<"byte"<<"contents of b="<<b<<endl;
+cout <<"size of float;"<<sizeof(float)<<"byte"<<"contents of c="<<c<<endl;
+cout <<"size of doble;" <<sizeof(double)<<"byte"<<"contents of d="<<d<<endl;
+cout <<"size of bool;"<<sizeof(bool)<<"byte"<<"contents of e="<<e<<endl;
+return 0;
+}
+
---
+++ lab3q_2.cpp
@@ -0,0 +1,21 @@
+//Library
+#include <iostream>
+using namespace std;
+//write the main function
+int main(){
+int a,b,sum,difference,multiply,modulus;
+a=3;
+b=4;
+sum=a+b;                     //doing sum
+difference=a-b;               //difference
+multiply=a*b;            //doing multiplication
+float division=(a/b);      //doing divion
+modulus=(a%b);            //taking modulus
+cout<<sum<<endl;           //sum will be performed and line will be changed
+cout<<difference<<endl;   //difference will be performed and line will be changed
+cout<<division<<  endl;        //division will be performed and line will be changed
+cout<<multiply<<endl;  //a and b will be multiplied and line will be changed
+cout<<modulus<<endl;        //modulus will be taken and line will be changed
+
+return 0;
+}
